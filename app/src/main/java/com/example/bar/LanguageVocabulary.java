7baa28d2e69ab34d13f26050c89e057c8e437749
emilRacz21 <emil.raczkiewicz@gmail.com>
2023-12-31 11:03:04 +0100

    commit

--- app/src/main/java/com/example/bar/LanguageVocabulary.java
+++ app/src/main/java/com/example/bar/LanguageVocabulary.java
@@ -20,7 +20,7 @@ public class LanguageVocabulary {
         spinnerChoose = new String[]{"Wybierz grupę","1A", "1B", "1C", "1D", "2A", "2B", "2C", "2D", "3A", "3B", "3C", "3D"};
         languageChoose =new String[]{"Polski", "Niemiecki", "Angielski", "Francuski","Rosyjski","Hiszpański"};
         home = new String[]{" DZISIEJSZE ZAJĘCIA ", "POPRZEDNI", "NASTĘPNY","Brak zajęć w podanym terminie", "ZOBACZ WSZYTSKIE ZAJĘCIA"};
-        options = new String[]{"Wybierz opcje", " Wybierz język "};
+        options = new String[]{"Wybierz opcje", "Wybierz język "};
         actionBarTitle = new String[]{" Strona główna ", " Zajęcia ", " Oceny ", " Edytuj profil ", " Zmień język ", " Więcej "};
         editProfileLang = new String[]{"Twój profil","Twój login", "Imię", "Nazwisko", "Telefon", "Dodatkowe informacje", "Hasło", "Powtórz hasło", "ZASTOSUJ", "USUŃ"};
         menu = new String[]{"Edytuj profil", "Zmień język", "Wyloguj się"};
@@ -35,7 +35,7 @@ public class LanguageVocabulary {
         spinnerChoose = new String[]{"Select group","1A", "1B", "1C", "1D", "2A", "2B", "2C", "2D", "3A", "3B", "3C", "3D"};
         languageChoose = new String[]{"Polish", "German", "English", "French", "Russian", "Spain"};
         home = new String[]{"TODAY'S CLASSES", "PREVIOUS", "NEXT", "No classes in the given term", "SEE ALL CLASSES"};
-        options = new String[]{" Choose options ", " Choose language "};
+        options = new String[]{" Choose options ", "Choose language "};
         actionBarTitle = new String[]{" Home ", " Lesson ", " Grades ", " change profile ", " change language ", " More options "};
         editProfileLang = new String[]{"Your profile", "Your Username", "First Name", "Last Name", "Phone", "Additional Information", "Password", "Repeat Password", "APPLY", "DELETE"};
         menu = new String[]{"Edit Profile", "Change Language", "Log Out"};
@@ -50,7 +50,7 @@ public class LanguageVocabulary {
         spinnerChoose = new String[]{"Gruppe auswählen","1A", "1B", "1C", "1D", "2A", "2B", "2C", "2D", "3A", "3B", "3C", "3D"};
         languageChoose = new String[]{"Polnisch", "Deutsch", "Englisch", "Französisch", "Russisch", "Spanisch"};
         home = new String[]{"HEUTIGER UNTERRICHT", "VORHERIGE", "NÄCHSTE", "Kein Unterricht im angegebenen Zeitraum", "ALLE KURSE ANZEIGEN"};
-        options = new String[]{"Optionen auswählen", "Sprache auswählen"};
+        options = new String[]{"Optionen auswählen", "Sprache wählen"};
         actionBarTitle = new String[]{" Startseite ", " Kurse ", " Noten ", " Profil bearbeiten ", " Sprache ändern ", " Mehr "};
         editProfileLang = new String[]{"Informationen", "Dein Benutzername", "Vorname", "Nachname", "Telefon", "Zusätzliche Informationen", "Passwort", "Passwort wiederholen", "ANWENDEN", "LÖSCHEN"};
         menu = new String[]{"Profil bearbeiten", "Sprache ändern", "Abmelden"};
@@ -65,7 +65,7 @@ public class LanguageVocabulary {
         spinnerChoose = new String[]{"Choisissez un groupe", "1A", "1B", "1C", "1D", "2A", "2B", "2C", "2D", "3A", "3B", "3C", "3D"};
         languageChoose = new String[]{"Polonais", "Allemand", "Anglais", "Français", "Russe", "Espagnol"};
         home = new String[]{" COURS D'AUJOURD'HUI ", "PRÉCÉDENT", "SUIVANT", "Aucun cours à la date spécifiée", "VOIR TOUS LES COURS"};
-        options = new String[]{"Choisissez une option", " Choisissez une langue "};
+        options = new String[]{"Choisissez une option", "Une langue "};
         actionBarTitle = new String[]{" Page d'accueil ", " Cours ", " Notes ", " Modifier le profil ", " Changer de langue ", " Plus "};
         editProfileLang = new String[]{"Informations", "Votre nom d'utilisateur", "Prénom", "Nom de famille", "Téléphone", "Informations supplémentaires", "Mot de passe", "Répéter le mot de passe", "APPLIQUER", "ANNULER"};
         menu = new String[]{"Modifier le profil", "Changer de langue", "Déconnexion"};
@@ -81,7 +81,7 @@ public class LanguageVocabulary {
         spinnerChoose = new String[]{"Выберите группу","1А", "1Б", "1В", "1Г", "2А", "2Б", "2В", "2Г", "3А", "3Б", "3В", "3Г"};
         languageChoose = new String[]{"Польский", "Немецкий", "Английский", "Французский","Русский","Испанский"};
         home = new String[]{" ЗАНЯТИЯ СЕГОДНЯ ", "ПРЕДЫДУЩИЕ", "СЛЕДУЮЩИЕ","Нет занятий на указанную дату", "ПОСМОТРЕТЬ ВСЕ ЗАНЯТИЯ"};
-        options = new String[]{"Выберите опцию", " Выберите язык "};
+        options = new String[]{"Выберите опцию", "Выберите язык "};
         actionBarTitle = new String[]{" Главная страница ", " Занятия ", " Оценки ", " Редактировать профиль ", " Изменить язык ", " Еще "};
         editProfileLang = new String[]{"информация","Ваш логин", "Имя", "Фамилия", "Телефон", "Дополнительная информация", "Пароль", "Повторите пароль", "ПРИМЕНИТЬ", "ОТМЕНА"};
         menu = new String[]{"Редактировать профиль", "Изменить язык", "Выйти"};
@@ -98,7 +98,7 @@ public class LanguageVocabulary {
         spinnerChoose = new String[]{"Selecciona un grupo", "1A", "1B", "1C", "1D", "2A", "2B", "2C", "2D", "3A", "3B", "3C", "3D"};
         languageChoose = new String[]{"Polaco", "Alemán", "Inglés", "Francés", "Ruso", "Español"};
         home = new String[]{" CLASES DE HOY ", "ANTERIOR", "SIGUIENTE", "Sin clases en el horario especificado", "VER TODAS LAS CLASES"};
-        options = new String[]{"Selecciona una opción", " Selecciona un idioma "};
+        options = new String[]{"Selecciona una opción", "Un idioma "};
         actionBarTitle = new String[]{" Página principal ", " Clases ", " Calificaciones ", " Editar perfil ", " Cambiar idioma ", " Más "};
         editProfileLang = new String[]{"Información", "Tu nombre de usuario", "Nombre", "Apellido", "Teléfono", "Información adicional", "Contraseña", "Repetir contraseña", "APLICAR", "CANCELAR"};
         menu = new String[]{"Editar perfil", "Cambiar idioma", "Cerrar sesión"};
